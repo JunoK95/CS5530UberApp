e85a630bc2a2d3aa470d1f8137a6d0f2d569c79f
Jaden Holladay <holladaystudio@gmail.com>
2018-03-25 21:17:39 -0600

    Check if UD is available before adding ride

--- classes/cs5530/Models/UC.java
+++ classes/cs5530/Models/UC.java
@@ -13,6 +13,25 @@ import java.util.*;
 
 public class UC
 {
+    /**
+     * Returns true if the UC is/was available for the given time slot.
+     */
+    public static boolean IsAvailable(String vin, String begin, String end)
+    {
+        /*
+        SELECT *
+        FROM UC uc, UD ud, Available a, Period p
+        WHERE uc.login=ud.login AND a.login=ud.login AND p.pid=a.pid AND uc.vin="12345"
+        HAVING p.begin < "2018-03-24 14:00:50" AND (p.end > '2018-03-24 16:35:50' OR p.end IS NULL)
+         */
+        String sql = String.format("SELECT *\n" +
+                "FROM UC uc, UD ud, Available a, Period p\n" +
+                "WHERE uc.login=ud.login AND a.login=ud.login AND p.pid=a.pid AND uc.vin=\"%s\"" +
+                "HAVING p.begin < \"%s\" AND (p.end > \"%s\" OR p.end IS NULL)", vin, begin, end);
+        String res = Database.Main().RunQuery(sql);
+        return res != null;
+    }
+
     public static JSONObject Create(HashMap<String, String> fields) throws ModelFailed
     {
         String[] requiredFields = new String[]{"vin", "category", "make", "model", "year", "login"};
